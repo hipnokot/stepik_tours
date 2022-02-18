36f8de678788ae09198a77addc3b302102bb00a2
hipnokot <99400974+hipnokot@users.noreply.github.com>
2022-02-18 09:43:56 +0400

    Add files via upload

---
+++ tours/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseServerError
+
+
+def main_view(request):
+    return render(request, "tours/index.html")
+
+
+def departure_view(request, departure):
+    return render(request, "tours/departure.html")
+
+
+def tour_view(request, tour_id):
+    return render(request, "tours/tour.html")
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ой, что-то сломалось... Простите-извините')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ошибка Сервера')
